1c6886b91bdec79a7086f94c6787dcf681fd07fd
Adila Bendahou <adilabendahou98@gmail.com>
2022-11-20 02:28:21 +0100

    Left side of the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header
+      className="sticky top-0 z-50 grid 
+    grid-flow-cols-3 bg-white shadow-md p-5 md:px-10"
+    >
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      <div></div>
+      <div></div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
