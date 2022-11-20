e401da391bebd61e9221dcad1bdb0666adbf3f05
Adila Bendahou <adilabendahou98@gmail.com>
2022-11-20 03:51:18 +0100

    Right Side of the Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
-import { SearchIcon } from '@heroicons/react/solid';
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  SearchIcon,
+  UserCircleIcon,
+} from '@heroicons/react/solid';
 
 const Header = () => {
   return (
@@ -15,6 +20,7 @@ const Header = () => {
           objectPosition="left"
         />
       </div>
+
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
@@ -26,7 +32,16 @@ const Header = () => {
          text-white rounded-full p-2 cursor-pointer md:mx-2"
         />
       </div>
-      <div></div>
+
+      <div className="flex  items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline  cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+
+        <div className="flex space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 };
