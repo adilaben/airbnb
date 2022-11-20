c0bf4fd439ad52a1553eb0a40d056b55b08f6066
Adila Bendahou <adilabendahou98@gmail.com>
2022-11-20 03:20:08 +0100

    SearchBar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 const Header = () => {
   return (
     <header
       className="sticky top-0 z-50 grid 
-    grid-flow-cols-3 bg-white shadow-md p-5 md:px-10"
+        grid-cols-3 bg-white shadow-md p-5 md:px-10"
     >
+      //left side: logo
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src="https://links.papareact.com/qd3"
@@ -14,7 +16,19 @@ const Header = () => {
           objectPosition="left"
         />
       </div>
-      <div></div>
+      //searchBar
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        />
+        <MagnifyingGlassIcon
+          className=" hidden md:inline-flex h-8  bg-red-400
+         text-white rounded-full p-2 cursor-pointer md:mx-2"
+        />
+      </div>
+      //right side
       <div></div>
     </header>
   );
