544f9c2ab726b648a8aaa284b188521cdff6c6aa
Adila Bendahou <adilabendahou98@gmail.com>
2022-11-24 21:02:26 +0100

    Banner Adjustment

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -4,7 +4,11 @@ import Image from 'next/legacy/image';
 const Banner = () => {
   return (
     <div className="relative top-0 h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[660px] ">
-      <Carousel indicators={false} className="[&>:nth-child(1)]:rounded-none">
+      <Carousel
+        indicators={false}
+        controls={false}
+        className="[&>:nth-child(1)]:rounded-none"
+      >
         <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white rounded-none">
           <Image
             src="/assets/banner1.jpg"
@@ -35,8 +39,8 @@ const Banner = () => {
       </Carousel>
       <div class="absolute inset-0 w-full h-full bg-gray-700 bg-opacity-50 "></div>
 
-      <div className="absolute top-[20%] sm:top-[35%] lg:top-[40%]   w-full text-center">
-        <p className="mb-4 text-4xl shadow-md font-extrabold leading-none md:text-5xl lg:text-6xl text-white ">
+      <div className="absolute top-[25%] sm:top-[30%] lg:top-[40%]   w-full text-center">
+        <p className="mb-4 text-3xl sm:text-4xl drop-shadow-lg font-extrabold leading-none md:text-5xl lg:text-6xl text-white ">
           Live anywhere anytime.
         </p>
         <p class="sm:mb-8 mx-5  text-[1rem] sm:text-lg font-normal text-gray-300 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
@@ -45,7 +49,7 @@ const Banner = () => {
         </p>
 
         <button
-          className="text-red-400 bg-white px-10 py-4  
+          className="text-red-400 bg-white px-10 py-3  
         rounded-full shadow-md font-bold my-3 hover:shadow-xl
         active:scale-90 transition duration-150"
         >
